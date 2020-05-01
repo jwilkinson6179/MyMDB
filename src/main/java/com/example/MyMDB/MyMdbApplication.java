019d4bf0ca97f5d03df039a08c8cb73c64b338af
jwilkinson6179 <JamesW6179@gmail.com>
2020-04-30 21:15:16 -0400

    Spring project initialized

---
+++ src/main/java/com/example/MyMDB/MyMdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyMDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyMDB/MyMdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyMDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
